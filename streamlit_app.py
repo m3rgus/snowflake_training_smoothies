6533588f9857fd64c0ba3ceaf3774d22e2244791
m3rgus <37961357+m3rgus@users.noreply.github.com>
2026-08-13 15:48:09 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,5 +41,5 @@ smoothiefroot_response = requests.get(
     "https://my.smoothiefroot.com/api/fruit/watermelon"
 )
 
-st.write(smoothiefroot_response.json())
-st.text(smoothiefroot_response)
+#st.write(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
